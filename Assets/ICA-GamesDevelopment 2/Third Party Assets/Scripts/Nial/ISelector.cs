ff29921c26d9aef6b44c96363a6454ff2a616298
Marek Martinak <123459150+Aang1214@users.noreply.github.com>
2023-12-29 22:12:28 +0000

    AddMovementForAgent
    I added movement from youtube tutorial

---
+++ Assets/ICA-GamesDevelopment 2/Third Party Assets/Scripts/Nial/ISelector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISelector
+{
+    void Check(Ray ray);
+
+    Transform GetSelection();
+
+    RaycastHit GetHitInfo();
+}
